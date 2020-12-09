1c8055e8a40de0d001b42713556855692752863c
Michał Świgost <mike@data.pl>
2020-12-09 12:12:49 +0100

    add calculator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,42 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theFormerUser");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theFormerUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator();
+        int resultOne = calculator.add(45, 67);
+        int resultTwo = calculator.substract(456, 122);
+        System.out.println(resultOne);
+        System.out.println(resultTwo);
+
+        System.out.println("test jednostkowy metody dodawania");
+        int testResultAdd = 112;
+        if(resultOne == testResultAdd) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("error!");
+        }
+
+        System.out.println("test jednostkowy metody odejmowania");
+        int testResultSubst = 334;
+        if(resultTwo == testResultSubst) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("error!");
+        }
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    int a;
+    int b;
+    int resultAdd;
+    int resultSubstract;
+
+    public int add(int a, int b) {
+        return resultAdd = a + b;
+    }
+
+    public int substract(int a, int b) {
+        return resultSubstract = a - b;
+    }
+
+}
